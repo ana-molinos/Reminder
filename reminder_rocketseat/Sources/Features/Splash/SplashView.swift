670bb6c189d5f6d78be5455ffd25927f34642b26
Ana Julia <anamolinos52@gmail.com>
2026-07-09 14:22:54 -0300

    add: gitignore and splash screen initial implementation

---
+++ reminder_rocketseat/Sources/Features/Splash/SplashView.swift
@@ -0,0 +1,34 @@
+//
+//  SplashView.swift
+//  reminder_rocketseat
+//
+//  Created by Ana Julia Molinos Leite Da Silva on 09/07/26.
+//
+
+import Foundation
+import UIKit
+
+class SplashView: UIView{
+    init(){
+        super.init(frame: .zero)
+        
+        setupUI()
+    }
+    
+    // Init padrão para caso algo de errado
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = .green
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+//        NSLayoutConstraint.activate([
+//            
+//        ])
+    }
+}
---
+++ reminder_rocketseat/Sources/Features/Splash/SplashViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SplashViewController.swift
+//  reminder_rocketseat
+//
+//  Created by Ana Julia Molinos Leite Da Silva on 08/07/26.
+//
+
+import Foundation
+import UIKit
+
+// Essa classe herda de UIViewController
+class SplashViewController: UIViewController {
+    let contentView = SplashView()
+    
+    // MARK: - Ciclo de vida da tela
+    
+    // sobrescrevendo método da superclasse
+    override func viewDidLoad() {
+        super.viewDidLoad() // construtor da classe pai
+        setup()
+        
+    } // Chamada após a view do controler ter sido carregada em memória (primeira coisa que aparece)
+    
+    private func setup(){
+        self.view.addSubview(contentView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        // Ancora da splash fica alinhada com a ancora da view do VC
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        // todas as propriedades precisam ter essa flag desmarcada para não serem interpretadas como um story board
+    }
+}
