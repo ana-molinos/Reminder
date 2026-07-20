3edff41499efbf3979c98397e4ce138143528c58
Ana Julia <anamolinos52@gmail.com>
2026-07-20 16:15:55 -0300

    add: installation of Nunito font

--- reminder_rocketseat/Sources/Features/DesignSystem/Typography.swift
+++ reminder_rocketseat/Sources/Features/DesignSystem/Typography.swift
@@ -9,10 +9,10 @@ import Foundation
 import UIKit
 
 public struct Typography {
-    static let heading = UIFont(name: "NunitoSans-ExtraBold", size: 20)
-    static let subheading = UIFont(name: "NunitoSans-ExtraBold", size: 16)
-    static let input = UIFont(name: "NunitoSans-Regular", size: 16)
-    static let label = UIFont(name: "NunitoSans-SemiBold", size: 14)
-    static let body = UIFont(name: "NunitoSans-Regular", size: 14)
-    static let tag = UIFont(name: "NunitoSans-Regular", size: 12)
+    static let heading = UIFont(name: "NunitoSans-12ptExtraLight_ExtraBold", size: 20)
+    static let subheading = UIFont(name: "NunitoSans-12ptExtraLight_ExtraBold", size: 16)
+    static let input = UIFont(name: "NunitoSans-12ptExtraLight_Regular", size: 16)
+    static let label = UIFont(name: "NunitoSans-12ptExtraLight_SemiBold", size: 14)
+    static let body = UIFont(name: "NunitoSans-12ptExtraLight_Regular", size: 14)
+    static let tag = UIFont(name: "NunitoSans-12ptExtraLight_Regular", size: 12)
 }
