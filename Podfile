03d801c6d8319ea867b2267f38392746fc175d69
Ana Julia <anamolinos52@gmail.com>
2026-07-24 15:50:34 -0300

    add: firebase package installed using cocoapods and authentication initial implementation

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'reminder_rocketseat' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for reminder_rocketseat
+  pod 'Firebase/Auth'
+
+end
--- reminder_rocketseat/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ reminder_rocketseat/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -6,9 +6,19 @@
 //
 
 import Foundation
+import Firebase
 
 class LoginBottomSheetViewModel {
     func doAuth(username: String, password: String){
         print("username: " + username)
+        
+        Auth.auth().signIn(withEmail: username, password: password) { [weak self] authResult, error in
+            
+            if let error = error {
+                print("Falha na autenticação: \(error)")
+            } else {
+                print(authResult, "Sucesso na autenticação")
+            }
+        }
     }
 }
