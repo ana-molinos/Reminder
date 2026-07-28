c01d814fc55afe4db0968df557dff928adda7558
Ana Julia <anamolinos52@gmail.com>
2026-07-28 15:45:39 -0300

    add: factory pattern to create instances of views

---
+++ reminder_rocketseat/Sources/Features/DependencyInjections/ViewControllersFactory/ViewControllersFactory.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllersFactory.swift
+//  reminder_rocketseat
+//
+//  Created by Ana Julia Molinos Leite Da Silva on 28/07/26.
+//
+
+import Foundation
---
+++ reminder_rocketseat/Sources/Features/DependencyInjections/ViewControllersFactory/ViewControllersFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllersFactoryProtocol.swift
+//  reminder_rocketseat
+//
+//  Created by Ana Julia Molinos Leite Da Silva on 28/07/26.
+//
+
+import Foundation
